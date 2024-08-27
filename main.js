23db6cb8673250f65917960f9899444a329a92b0
isabelymacedo <147161750+isabelymacedo@users.noreply.github.com>
2024-08-27 10:54:51 -0300

    rgrg

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ const caixaPergunta = document.querySelectorAll (".caixa-perguntas")
 const caixaAlternativas = document.querySelectorAll (".caixa-alternativas")
 const caixaResultado = document.querySelectorAll (".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    }
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    }
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    }
+    {
+        enunciado: "Pergunta 4",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    } 
+]
