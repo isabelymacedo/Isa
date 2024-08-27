02b8d713c44ce67a700b43172fbd455d8eae4a21
isabelymacedo <147161750+isabelymacedo@users.noreply.github.com>
2024-08-27 07:47:50 -0300

    irfir4f

---
+++ main.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document.querySelectorAll(".caixa-principal")
+const caixaPergunta = document.querySelectorAll (".caixa-perguntas")
+const caixaAlternativas = document.querySelectorAll (".caixa-alternativas")
+const caixaResultado = document.querySelectorAll (".caixa-resultado")
